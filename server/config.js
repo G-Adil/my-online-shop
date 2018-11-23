ccd549ee5a0f79d4abee19bce6534da5f99af743
G-Adil <g.adil@users.noreply.github.com>
2018-11-23 18:18:03 +0100

    Hello MERN!

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://vendetta:Vendetta28@ds115434.mlab.com:15434/my-online-shop',
   port: process.env.PORT || 8000,
 };
 
